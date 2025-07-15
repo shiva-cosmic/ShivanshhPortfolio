0c6ede645ecf7abf3cd0a5d294ac624d2bac62d7
Shivanshh369 <shivanshhh369@gmail.com>
2025-07-15 14:11:09 +0530

    Header & NavBar Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
 /*=============== MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle");
 
 /* Menu show - hidden */
-
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show-menu");
+  navToggle.classList.toggle("animate-toggle");
+});
 /*=============== REMOVE MENU MOBILE ===============*/
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
