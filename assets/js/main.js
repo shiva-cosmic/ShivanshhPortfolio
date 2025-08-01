d4aa8574a0250fc3997dc15f66256db125e1f813
Shivanshh369 <shivanshhh369@gmail.com>
2025-08-01 16:55:49 +0530

    Breakpoint added

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,20 @@ var servicesSwiper = new Swiper(".services-swiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
 });
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
