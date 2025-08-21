e51840006b316ab853fca8da9ecbab6e032042a3
Shivanshh369 <shivanshhh369@gmail.com>
2025-08-21 23:03:36 +0530

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,28 +51,6 @@ const scrollActive = () => {
 };
 
 window.addEventListener("scroll", scrollActive);
-const sections = document.querySelectorAll("section[id]");
-
-const scrollActive = () => {
-  const scrollY = window.pageYOffset;
-
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    sectionTop = current.offsetTop - 58;
-    (sectionId = current.getAttribute("id")),
-      (sectionClass = document.querySelector(
-        ".nav-menu a[href*=" + sectionId + "]"
-      ));
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      sectionClass.classList.add("active-link");
-    } else {
-      sectionClass.classList.remove("active-link");
-    }
-  });
-};
-
-window.addEventListener("scroll", scrollActive);
 
 /*=============== SERVICES SWIPER ===============*/
 var servicesSwiper = new Swiper(".services-swiper", {
