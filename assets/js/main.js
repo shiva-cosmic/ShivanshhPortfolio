c252882a0df8d1cdffe7de2ee67c5a7c16d93719
Shivanshh369 <shivanshhh369@gmail.com>
2025-11-18 14:30:34 +0530

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 /*=============== MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle");
 
 /* Menu show - hidden */
 navToggle.addEventListener("click", () => {
